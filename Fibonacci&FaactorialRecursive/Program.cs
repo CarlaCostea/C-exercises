b847147b5d47904f4f15450a339a7934796ac17e
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-01-22 12:43:17 +0200

    Recursivity

---
+++ Fibonacci&FaactorialRecursive/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        private static double Factorial(double n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+        private static int FibonacciResult(int n)
+        {
+            int previous = 0;
+            return Fibonacci(n, ref previous);
+        }
+
+        private static int Fibonacci(int n, ref int previous)
+        {
+            const int min = 2;
+            if (n < min)
+            {
+                return n;
+            }
+
+            int beforePrevious = 0;
+            previous = Fibonacci(n - 1, ref beforePrevious);
+            return previous + beforePrevious;
+        }
+    }
+}
---
+++ ReplaceCharInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReplaceChar
+{
+    class Program
+    {
+        static void Main()
+        {
+            string initialString = Console.ReadLine();
+            char charToReplace = Convert.ToChar(Console.ReadLine());
+            string newChar = Console.ReadLine();
+            string newString = ReplaceChar(initialString, charToReplace, newChar);
+
+            Console.WriteLine(newString);
+        }
+
+        private static string ReplaceChar(string initialString, char charToReplace, string newChar)
+        {
+            if (initialString.Length < 1)
+            {
+                return initialString;
+            }
+
+            string result = "";
+            if (initialString[0] == charToReplace)
+            {
+                result = newChar;
+            }
+            else
+            {
+                result += initialString[0];
+            }
+
+            return result + ReplaceChar(initialString.Substring(1), charToReplace, newChar);
+        }
+    }
+}
